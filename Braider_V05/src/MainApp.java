1ea70d34a54df656f97faa090196efc0e0e25e4b
Unknown <stiganielsen@gmail.com>
2017-11-15 15:17:09 +0100

    hello aga

---
+++ Braider_V05/src/MainApp.java
@@ -0,0 +1,23 @@
+
+import processing.core.PApplet;
+
+public class MainApp extends PApplet{
+
+ public static void main(String[] args){
+     PApplet.main("MainApp", args);
+ }
+    public void settings() {
+size(200,200);
+    }
+    public void setup() {
+
+    }
+    public void draw() {
+fill(255,3,3);
+     ellipse(100,100,22,22);
+
+    }
+
+
+
+}
